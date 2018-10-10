379a75c325d755604eff8dea9724443ee061587c
Nicholas Kirkby <nkirkby@e1z1r5p39.42.us.org>
2018-10-10 14:16:03 -0700

    prototyping solver

--- srcs/ft_solve_bsq.c
+++ srcs/ft_solve_bsq.c
@@ -6,7 +6,7 @@
 /*   By: nkirkby <nkirkby@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/09 16:32:02 by seli              #+#    #+#             */
-/*   Updated: 2018/10/10 11:13:26 by nkirkby          ###   ########.fr       */
+/*   Updated: 2018/10/10 14:15:28 by nkirkby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,43 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void	ft_find_biggest_square(char);
+
+void	is_largest_square()
+
+/*
+**	grow the square if there is space on prior lines.  Returns a solution if
+**	and only if the square is bigger than the existing largest square.
+*/
+
+void	check_prior_lines(t_head *this_line, t_line *space)
+{
+
+}
+
+/*
+**	Iterate over all lines
+*/
+
+void	ft_find_biggest_square(t_parser_state *state)
+{
+	t_head	*this_line;
+	t_head	*prior_line;
+	t_line	*space;
+
+	head = state->head_list;
+	while (head)
+	{
+		space = head->line;
+		while (space)
+		{
+			// iterate up through previous head to see how big the square is.
+
+			if space->
+			space = space->next;
+		}
+		head = head->next;
+	}
+}
 
 int		ft_solve_bsq(char *filename)
 {

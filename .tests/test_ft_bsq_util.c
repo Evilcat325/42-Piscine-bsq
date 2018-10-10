6895279359bde72e13dbf4bda369c98033f334cc
Nicholas Kirkby <nkirkby@e1z1r5p39.42.us.org>
2018-10-10 02:18:12 -0700

    adding pretty absurd test cases to read_bsq_header

--- .tests/test_ft_bsq_util.c
+++ .tests/test_ft_bsq_util.c
@@ -1,21 +1,11 @@
+#include <stdio.h>
 #include "../srcs/ft_bsq_util.c"
 #include "../srcs/ft_stdio.c"
 #include "rag.h"
 
 int main()
 {
-	char header_1[] = "10000.ox";
-	char header_2[] = "12345678";
-	char header_3[] = "f00bar";
-	char header_4[] = "1af";
-	char header_5[] = "";
-	char header_6[] = "123\n";  // invalid
-	char header_7[] = "1234\n"; // valid
-	char header_8[] = "a234\n"; // invalid
-	char header_8[] = "+2345\n"; // valid
-	char header_8[] = "-2345\n"; // valid
-
-	int err;
+	int ret;
 
 	t_file_info	info;
 	info.empty = 0;
@@ -24,26 +14,56 @@ int main()
 	info.obstacle = 0;
 	info.width = 0;
 
-	err = read_bsq_header(header_1, &info);
-	ASSERT_EQUAL(err, 0);
+	char header_1[] = "10000.ox\n";
+	ret = read_bsq_header(header_1, &info);
+	assert(ret == 9);
 	ASSERT_EQUAL(info.empty, '.');
 	ASSERT_EQUAL(info.obstacle, 'o');
 	ASSERT_EQUAL(info.full, 'x');
 	ASSERT_EQUAL(info.height, 10000);
 
-	err = read_bsq_header(header_2, &info);
-	ASSERT_EQUAL(err, 0);
+	char header_2[] = "12345678\n";
+	ret = read_bsq_header(header_2, &info);
+	assert(ret == 9);
 	ASSERT_EQUAL(info.empty, '6');
 	ASSERT_EQUAL(info.obstacle, '7');
 	ASSERT_EQUAL(info.full, '8');
 	ASSERT_EQUAL(info.height, 12345);
 
-	err = read_bsq_header(header_3, &info);
-	ASSERT_NOT_EQUAL(err, 0);
+	char header_2_no_newline[] = "12345678";
+	ret = read_bsq_header(header_2_no_newline, &info);
+	assert(ret < 0);
 
-	err = read_bsq_header(header_4, &info);
-	ASSERT_NOT_EQUAL(err, 0);
+	char header_3[] = "f00bar";
+	ret = read_bsq_header(header_3, &info);
+	assert(ret < 0);
 
-	err = read_bsq_header(header_5, &info);
-	ASSERT_NOT_EQUAL(err, 0);
+	char header_4[] = "1af";
+	ret = read_bsq_header(header_4, &info);
+	assert(ret < 0);
+
+	char header_5[] = "";
+	ret = read_bsq_header(header_5, &info);
+	assert(ret < 0);
+
+	char header_6[] = "123\n";  // invalid
+	ret = read_bsq_header(header_6, &info);
+	assert(ret < 0);
+
+	char header_7[] = "1234\n"; // valid
+	ret = read_bsq_header(header_7, &info);
+	ASSERT_EQUAL(info.height, 1);
+	assert(ret == 5);
+
+	char header_8[] = "a234\n"; // invalid
+	ret = read_bsq_header(header_8, &info);
+	assert(ret < 0);
+
+	char header_9[] = "+2345\n"; // valid
+	ret = read_bsq_header(header_9, &info);
+	assert(ret == 6);
+
+	char header_10[] = "-2345\n"; // invalid
+	ret = read_bsq_header(header_10, &info);
+	assert(ret < 0);
 }
--- srcs/ft_bsq_util.c
+++ srcs/ft_bsq_util.c
@@ -6,14 +6,25 @@
 /*   By: nkirkby <nkirkby@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/09 20:53:18 by nkirkby           #+#    #+#             */
-/*   Updated: 2018/10/10 00:57:41 by nkirkby          ###   ########.fr       */
+/*   Updated: 2018/10/10 02:17:27 by nkirkby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_bsq.h"
 
+int		map_characters_are_not_unique(t_file_info *info)
+{
+	if (info->full != info->obstacle &&
+		info->obstacle != info->empty &&
+		info->empty != info->full)
+		return (0);
+	return (-1);
+}
+
 /*
-**	Returns the index of the end of the header if valid.  -1 otherwise.
+**	If the header is valid, this function returns the starting index of the
+**	first line after the header.
+**	If the header is invalid, -1.
 */
 
 int		read_bsq_header(char *str, t_file_info *info)
@@ -21,25 +32,23 @@ int		read_bsq_header(char *str, t_file_info *info)
 	char	line_number_descriptor[11];
 	short	end_of_line;
 	short	i;
-	char	err;
 
 	end_of_line = 0;
-	while (str[end_of_line] && str[end_of_line] != '\n')
-		end_of_line++;
-	if (end_of_line < 4)
-		return (-1);
+	while (str[end_of_line] != '\n')
+		if (str[end_of_line++] == 0)
+			return (-1);
 	i = end_of_line;
 	info->full = str[--i];
 	info->obstacle = str[--i];
 	info->empty = str[--i];
-	line_number_descriptor[--i] = '\0';
-	while (i >= 0)
-	{
-		line_number_descriptor[i] = str[i];
-		i--;
-	}
-	err = ft_atoi_ptr(line_number_descriptor, &(info->height));
-	if (err)
+	if (map_characters_are_not_unique(info))
 		return (-1);
-	return (end_of_line);
+	line_number_descriptor[i - 1] = '\0';
+	while (--i >= 0)
+		line_number_descriptor[i] = str[i];
+	if (ft_atoi_ptr(line_number_descriptor, &(info->height)))
+		return (-1);
+	if ((end_of_line < 4) || info->height <= 0)
+		return (-1);
+	return (end_of_line + 1);
 }

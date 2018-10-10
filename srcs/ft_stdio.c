89f71de10419bc5bec7cc7d8e728ded345c2b623
Nicholas Kirkby <nkirkby@e1z1r5p39.42.us.org>
2018-10-09 20:33:28 -0700

    adding ft_atoi to ft_stdio

--- srcs/ft_stdio.c
+++ srcs/ft_stdio.c
@@ -3,11 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   ft_stdio.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: nkirkby <nkirkby@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/09 20:31:37 by seli              #+#    #+#             */
-/*   Updated: 2018/10/09 20:31:48 by seli             ###   ########.fr       */
+/*   Updated: 2018/10/09 20:33:06 by nkirkby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stdio.h"
+
+#define IS_DIGIT(X) (X >= '0' && X <= '9')
+#define IS_SIGN(X) (X == '+' || X == '-')
+
+/*
+** The real atoi:
+** - parses + and - signs
+** - ignores leading zeros
+** - starts parsing if it encounters a + sign, - sign,
+** - stops parsing if it encounters a non-digit
+** - overflows if the number is larger than an int
+*/
+
+int	ft_atoi(char *str)
+{
+	long	output;
+	int		sign;
+
+	output = 0;
+	sign = 1;
+	if (!(IS_DIGIT(*str) || IS_SIGN(*str)))
+		return (0);
+	if (IS_SIGN(*str))
+		sign = *str++ == '-' ? -1 : 1;
+	if (!IS_DIGIT(*str))
+		return (0);
+	while (*str == '0')
+		str++;
+	while (*str && IS_DIGIT(*str))
+	{
+		output *= 10;
+		output += (int)(*str++ - '0');
+	}
+	output *= sign;
+	return (int)(output);
+}

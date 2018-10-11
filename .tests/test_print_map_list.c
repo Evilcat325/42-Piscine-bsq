faf2e66155d188b1dfe3a95d2ab7b7de76cdf32d
Nicholas Kirkby <nkirkby@e1z1r5p39.42.us.org>
2018-10-10 21:12:31 -0700

    moving ft_print_map to tests dir

--- .tests/test_print_map_list.c
+++ .tests/test_print_map_list.c
@@ -6,7 +6,7 @@
 /*   By: nkirkby <nkirkby@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/09 21:31:51 by seli              #+#    #+#             */
-/*   Updated: 2018/10/10 14:54:29 by nkirkby          ###   ########.fr       */
+/*   Updated: 2018/10/10 21:12:06 by nkirkby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,42 @@ void	ft_print_map_list(t_parser_state *state)
 			node = node->next;
 		}
 		printf("\n");
+		line++;
+	}
+}
+
+void	ft_print_map(t_parser_state *state)
+{
+	int			line;
+	int			i;
+	t_spacenode	*node;
+
+	line = 0;
+	while (line < state->line_number)
+	{
+		node = state->lines[line].nodes;
+		i = 0;
+		while (node)
+		{
+			while (i < node->index)
+			{
+				write(1, &state->file_info.obstacle, 1);
+				i++;
+			}
+			while (i < node->index + node->length)
+			{
+				write(1, &state->file_info.empty, 1);
+				i++;
+			}
+			node = node->next;
+		}
+		while (i < state->file_info.width)
+		{
+			write(1, &state->file_info.obstacle, 1);
+			i++;
+		}
+		if (line < state->line_number - 1)
+			write(1, "\n", 1);
 		line++;
 	}
 }
